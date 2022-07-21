944f015a41d5579446681fd67a3e928bc8c267af
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-07-21 11:27:24 -0400

    final commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Linq;
+
+
+List<string> videoGames = new List<string>{ "Elden Ring", "League of Legends", "MineCraft", "OSRS" };
+
+    videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
